ce87884513e0ee1839c81359058bb644cd242dca
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2022-03-21 09:40:16 -0300

    eslint added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.color};
   background-color: ${({ theme }) => theme.backgroundColor};
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return <Title>harleyjayy_xo</Title>
+  return <Title>harleyjayy_xo</Title>;
 }
