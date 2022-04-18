f4227a958c9bc98a3672835aca380425fcf67d34
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2022-04-18 00:17:46 -0300

    removing useless import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-
 import HomeScreen from '../src/screens/HomeScreen';
 import websitePageHOC from '../src/wrappers/websitePage/hoc';
 
